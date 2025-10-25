42197ddc4b3bd85c491d22f7f30a60406aa93815
Diego Ortiz <dfortiz116@gmail.com>
2025-10-25 00:27:37 -0500

    Mejoras en el Home - RandJokes implementado

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { counterPageComponent } from './pages/counter/counter';
 import { homePageComponent } from './pages/home/home';
+import { randjokesPageComponent } from './pages/randJokes/randJokes';
 
 export const routes: Routes = [
   {
@@ -10,5 +11,9 @@ export const routes: Routes = [
   {
     path: 'counter',
     component: counterPageComponent,
+  },
+  {
+    path: 'jokes',
+    component: randjokesPageComponent,
   }
 ];
