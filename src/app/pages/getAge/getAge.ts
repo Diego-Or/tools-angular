4d01de4c0ee9bac978d9f74c3f5213c194e3761b
Diego Ortiz <dfortiz116@gmail.com>
2025-10-31 11:38:01 -0500

    Función de getAge implementado

--- src/app/pages/getAge/getAge.ts
+++ src/app/pages/getAge/getAge.ts
@@ -1,20 +1,37 @@
-import { Component, OnInit, signal } from '@angular/core';
+import { Component, input, signal, output} from '@angular/core';
 import { DatePipe } from "@angular/common";
 
 @Component({
   selector: 'getAge',
   templateUrl: 'getAge.html',
-  styleUrl: 'getAge.scss'
+  styleUrls: ['./../../app.scss'],
+  styles: '.InputFecha{max-width: 400px;}'
 })
 
 export class getAgeComponent{
-  constructor() { }
-  inputFecha = 'prueba';
-  fecha: string = '';
 
-  getAge(): string{
-    console.log(this.fecha);
-    return `hola`
+  fechaActual: Date = new Date();
+  fechaNacimiento = signal('');
+  edad = signal(0);
+
+  getAge(){
+    // Convertir la fecha de nacimiento (string) a tipo Date
+    const nacimiento = new Date(this.fechaNacimiento());
+
+    // Calcular la diferencia de años
+    let edad = this.fechaActual.getFullYear() - nacimiento.getFullYear();
+
+    // Ajustar si aún no ha cumplido años este año
+    const mesActual = this.fechaActual.getMonth();
+    const diaActual = this.fechaActual.getDate();
+    const mesNacimiento = nacimiento.getMonth();
+    const diaNacimiento = nacimiento.getDate();
+
+    if (mesActual < mesNacimiento || (mesActual === mesNacimiento && diaActual < diaNacimiento)) {
+      edad--;
+    }
+
+    this.edad.set(edad);
   }
 
 }
